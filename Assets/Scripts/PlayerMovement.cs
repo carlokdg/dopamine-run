8cd911a687fe5f60d47c824298866644eb1b7d4e
carlokdg <carlo.virdis@student.kdg.be>
2025-10-15 14:10:51 +0200

    Add code to make sure the player doesn't go out of bounds
    Add the code in the player script to make sure the player doesn't go out of bounds

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,33 @@ using System;
 // This is the script that controls the player's forward movement and speed.
 public class PlayerMovement : MonoBehaviour
 {
+
+    //Player Speed and Player Horizontal Speed
     public float playerSpeed = 2;
     public float horizontalSpeed = 3;
 
+    public float rightlimit = 5.5f;
+    public float leftlimit = -5.5f;
+
     void Update()
     {
+
+        // This part makes the player move forward constantly and allows for left and right movement using A/D or Left/Right Arrow keys.
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
-        } 
-        if (Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.RightArrow))
+            if (this.gameObject.transform.position.x > leftlimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            if (this.gameObject.transform.position.x < rightlimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            }
         }
     }
 }

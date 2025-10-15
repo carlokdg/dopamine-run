8d249396b943cdec9d0efc523826a580a9af65b6
carlokdg <carlo.virdis@student.kdg.be>
2025-10-15 13:50:44 +0200

    Player move Left & Right, also with arrows
    Player move Left & Right, also with arrows

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,23 @@ using System.Xml;
 using UnityEngine;
 using System;
 
+// This is the script that controls the player's forward movement and speed.
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 2;
+    public float horizontalSpeed = 3;
+
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+        } 
+        if (Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+        }
     }
 }

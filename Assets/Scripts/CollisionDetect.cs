ea6b88eeccffd03ac2b047aa91e2ec6a6d458ba0
carlokdg <carlo.virdis@student.kdg.be>
2025-10-16 17:22:40 +0200

    FadeOut Animation + Camera FX script
    FadeOut Animation + Camera FX script animation,Super nice!

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class CollisionDetect : MonoBehaviour
 {
@@ -6,11 +8,24 @@ public class CollisionDetect : MonoBehaviour
 
     [SerializeField] GameObject thePlayer;
     [SerializeField] GameObject playerAnim;
-     void OnTriggerEnter(Collider other)
+    [SerializeField] AudioSource collisionFX;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject fadeOut;
+    void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(CollisionEnd());
+    }
+    
+    IEnumerator CollisionEnd()
+    {
+        collisionFX.Play();
         //Disable Player Movement Script
         thePlayer.GetComponent<PlayerMovement>().enabled = false;
         playerAnim.GetComponent<Animator>().Play("Stumble Backwards");
+        mainCam.GetComponent<Animator>().Play("CollisionCam");
+        yield return new WaitForSeconds(1);
+        fadeOut.SetActive(true);
+
     }
 
 }

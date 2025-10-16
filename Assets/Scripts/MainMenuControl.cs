b73663ee760750a2ada06019712fa2dda692fe58
carlokdg <carlo.virdis@student.kdg.be>
2025-10-16 21:42:04 +0200

    camera animation in the main menu + sound when you click the Button Start
    camera animation in the main menu + sound when you click the Button Start

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 public class MainMenuControl : MonoBehaviour
 {
     [SerializeField] GameObject fadeOut;
+    [SerializeField] GameObject bounceText;
+    [SerializeField] GameObject bigButton;
+    [SerializeField] GameObject animCam;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject menuControls;
+
+    [SerializeField] AudioSource buttonSelect;
+
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -15,19 +23,40 @@ public class MainMenuControl : MonoBehaviour
 
     }
 
+    public void MenuBeginButton()
+    {
+        StartCoroutine(AnimCam());
+
+    }
     public void StartGame()
     {
         StartCoroutine(StartButton());
 
     }
-    
-    
+
+
     IEnumerator StartButton()
     {
+        buttonSelect.Play();
         fadeOut.SetActive(true);
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(1);
-        
-    }   
 
+    }
+
+   
+    IEnumerator AnimCam()
+
+    
+    {
+        animCam.GetComponent<Animator>().Play("AnimCam");
+        bounceText.SetActive(false);
+        bigButton.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+        mainCam.SetActive(true);
+        animCam.SetActive(false);
+        menuControls.SetActive(true);
+        
+
+    }
 }

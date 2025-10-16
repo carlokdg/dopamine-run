822e8ae19743d666676ebc47302fa0285d9cea40
carlokdg <carlo.virdis@student.kdg.be>
2025-10-16 22:16:52 +0200

    Increase the speed while playing
    Increase the speed while playing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,14 +15,21 @@ public class PlayerMovement : MonoBehaviour
     public float rightlimit = 5.5f;
     public float leftlimit = -5.5f;
 
+    // Velocità con cui aumenta la velocità del player nel tempo
+    public float speedIncreaseRate = 0.1f; 
+
     void Update()
     {
+
+        // Aumenta gradualmente la velocità del player nel tempo
+        playerSpeed += speedIncreaseRate * Time.deltaTime;
 
         // This part makes the player move forward constantly and allows for left and right movement using A/D or Left/Right Arrow keys.
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            // Check to make sure the player doesn't go out of bounds
             if (this.gameObject.transform.position.x > leftlimit)
             {
                 transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
